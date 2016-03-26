0bd6faf598129a1041076aadb213969acb0c5265
stefanavram <sebastianavram@gmail.com>
2016-03-26 14:53:42 +0200

    modify js

--- html/map_markers.js
+++ html/map_markers.js
@@ -1,19 +1,25 @@
 // This example adds a marker to indicate the position of Bondi Beach in Sydney,
 // Australia.
-function initMap() {
-var map = new google.maps.Map(document.getElementById('map'), {
-  zoom: 4,
-  center: {lat: -33, lng: 151}
+var holes;
+$.getJSON("http://roadgems.ml/get_all_potholes.php", function(json){
+    holes = json;
 });
+console.log(holes);
+function initMap() {
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 4,
+        center: { lat: -33, lng: 151 }
+    });
 
-var image = 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png';
-new google.maps.Marker({
-  position: {lat: -33.890, lng: 151.274},
-  map: map,
-  icon: image
+    var image = 'http://www.google.com/intl/en_us/mapfiles/ms/micons/blue-dot.png';
+    new google.maps.Marker({
+            position: { lat: -33.890, lng: 151.274 },
+            map: map,
+            icon: image
+        }
+    );
 }
 
-);
+function drop() {
+	
 }
-
-
