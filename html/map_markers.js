857aea5750e1f1265c57395759b512662d004bbf
Stefan <sebastianavram@gmail.com>
2016-03-29 07:09:00 -0400

    updated map

--- html/map_markers.js
+++ html/map_markers.js
@@ -2,13 +2,14 @@ var holes;
 var image = 'http://www.google.com/intl/en_us/mapfiles/ms/micons/blue-dot.png';
 var markers = [];
 var map;
+var styles=[{"elementType":"geometry","stylers":[{"hue":"#ff4400"},{"saturation":-68},{"lightness":-4},{"gamma":0.72}]},{"featureType":"road","elementType":"labels.icon"},{"featureType":"landscape.man_made","elementType":"geometry","stylers":[{"hue":"#0077ff"},{"gamma":3.1}]},{"featureType":"water","stylers":[{"hue":"#00ccff"},{"gamma":0.44},{"saturation":-33}]},{"featureType":"poi.park","stylers":[{"hue":"#44ff00"},{"saturation":-23}]},{"featureType":"water","elementType":"labels.text.fill","stylers":[{"hue":"#007fff"},{"gamma":0.77},{"saturation":65},{"lightness":99}]},{"featureType":"water","elementType":"labels.text.stroke","stylers":[{"gamma":0.11},{"weight":5.6},{"saturation":99},{"hue":"#0091ff"},{"lightness":-86}]},{"featureType":"transit.line","elementType":"geometry","stylers":[{"lightness":-48},{"hue":"#ff5e00"},{"gamma":1.2},{"saturation":-23}]},{"featureType":"transit","elementType":"labels.text.stroke","stylers":[{"saturation":-64},{"hue":"#ff9100"},{"lightness":16},{"gamma":0.47},{"weight":2.7}]}];
+
 
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
-	zoom: 10, 
-        center: { lat: -33, lng: 151 }
+	zoom: 12, 
+        center: { lat: 45.759780, lng: 21.230020 }
     });
-    drop();
 
  if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position) {
@@ -17,18 +18,12 @@ function initMap() {
         lng: position.coords.longitude
       };
       map.setCenter(pos);
-    }, function() {
-      handleLocationError(true, infoWindow, map.getCenter());
     });
-  } else {
-    // Browser doesn't support Geolocation
-    handleLocationError(false, infoWindow, map.getCenter());
   }
+map.setOptions({styles: styles});
+drop();
 }
 
-function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-    console.log('Error: The Geolocation service failed. Error: Your browser doesn\'t support geolocation.');
-}
 
 
 function addMarkerWithTimeout(position, timeout) {
@@ -45,7 +40,6 @@ function drop() {
    $.getJSON("http://roadgems.ml/get_all_potholes.php", function(json){
 	
         holes = json["holes"];
-	console.log(holes);
 	for (var i = 0; i < holes.length; i++) {
 		addMarkerWithTimeout(holes[i].position, i*100);     
 	}
